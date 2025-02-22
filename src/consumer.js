1095a3003e3efff33450d51568e76f011a593ac7
AnggiIrawann <anggi.irawan@nexsoft.co.id>
2025-02-22 13:08:20 +0700

    create consumer export notes

---
+++ src/consumer.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const amqp = require('amqplib');
+const NotesService = require('./NotesService');
+const MailSender = require('./MailSender');
+const Listener = require('./listener');
+
+const init = async () => {
+  const notesService = new NotesService();
+  const mailSender = new MailSender();
+  const listener = new Listener(notesService, mailSender);
+
+  const queue = 'export:notes';
+  const connection = await amqp.connect(process.env.RABBITMQ_SERVER);
+  const channel = await connection.createChannel();
+  await channel.assertQueue(queue, {
+    durable: true,
+  });
+  await channel.consume(queue, listener.listen, { noAck: true });
+};
+
+init();
